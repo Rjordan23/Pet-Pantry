c2cae20f0c00f15f8d2540f346fa05d1e3b5faa9
Ramsey Jordan <Ramsey.Jordan.84@gmail.com>
2024-11-26 23:58:51 -0500

    Added nav toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,25 @@
 'use strict'
 
 const addEventOnElem = function (elem, type, callback) {
-    if (elem.length)
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
 }
+
+
+// Navbar Toggle
+
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
