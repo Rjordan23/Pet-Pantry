6c302be2e398d04a8890088678678c48ee4ad40e
Ramsey Jordan <Ramsey.Jordan.84@gmail.com>
2024-12-05 02:49:44 -0500

    Finished adding all sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,12 +36,15 @@ addEventOnElem(navbarLinks, "click", closeNavbar);
 /* Active Header when scroll @ 100px */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 const activeElemOnScroll = function () {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
